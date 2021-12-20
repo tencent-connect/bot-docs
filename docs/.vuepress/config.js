09dacb38f69c0d4301408d8b42be1515de3f35ef
zoswing <1141421776@qq.com>
2021-12-20 20:02:21 +0800

    perf(pwa): 添加pwa插件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -68,7 +68,7 @@ module.exports = {
       },
     ],
     ['vuepress-plugin-baidu-autopush'],
-    ['@vuepress/last-updated'],
+    ['@vuepress/pwa'],
   ],
   globalUIComponents: ['TuXiaoChao'],
   theme: require.resolve('./theme-qq'),
