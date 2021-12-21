4c820becef5f9ce4f7918137f8e81f2e696a3d72
zoswing <1141421776@qq.com>
2021-12-21 17:21:10 +0800

    fix(nodesdk): 完善私域机器人添加描述

--- docs/.vuepress/components/Warnning.vue
+++ docs/.vuepress/components/Warnning.vue
@@ -1,12 +1,11 @@
 <template>
-  <div class="custom-block danger"
-    ><p class="custom-block-title">注意</p>
-    <ul
-      ><li
-        >公域机器人暂不支持申请，仅私域机器人可用，私域机器人开发者请发邮件<a href="mailto:QQ_Bot@tencent.com">QQ_Bot@tencent.com</a>申请开通。</li
-      ></ul
-    ></div
-  >
+  <div class="custom-block danger">
+    <p class="custom-block-title">注意</p>
+    <ul>
+      <li>公域机器人暂不支持申请，仅私域机器人可用，私域机器人开发者请发邮件<a href="mailto:QQ_Bot@tencent.com">QQ_Bot@tencent.com</a>申请开通。
+        注意:开通后需要先将机器人从频道移除，然后重新添加，方可生效。</li>
+    </ul>
+  </div>
 </template>
 <script>
 export default {
