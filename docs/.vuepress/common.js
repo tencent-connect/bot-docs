3ab421218cc34313ddc03d5b146ba7af418a83e3
ostwindli <9837438@qq.com>
2021-12-24 00:27:30 +0800

    fix: process环境问题

--- docs/.vuepress/common.js
+++ docs/.vuepress/common.js
@@ -1,5 +1,5 @@
-console.log({ BUILD_ENV: process.env.BUILD_ENV });
+
 module.exports = {
   cdnBase: 'https://abc-pcweb-1251316161.file.myqcloud.com/',
-  cdnPath: process.env.BUILD_ENV === 'test' ? 'qq-test-channel/' : 'qq-channel/',
+  cdnPath: typeof process !== 'undefined' && process.env && process.env.BUILD_ENV === 'test' ? 'qq-test-channel/' : 'qq-channel/',
 };
