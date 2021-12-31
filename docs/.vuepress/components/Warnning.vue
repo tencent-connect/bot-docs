a8ac2150b8d41d8e30ac514f298f729b85721e80
zoswing <1141421776@qq.com>
2021-12-31 13:45:21 +0800

    docs: 修改特殊接口申请文案描述

--- docs/.vuepress/components/Warnning.vue
+++ docs/.vuepress/components/Warnning.vue
@@ -2,8 +2,8 @@
   <div class="custom-block danger">
     <p class="custom-block-title">注意</p>
     <ul>
-      <li>公域机器人暂不支持申请，仅私域机器人可用，私域机器人开发者请发邮件<a href="mailto:QQ_Bot@tencent.com">QQ_Bot@tencent.com</a>申请开通。
-        注意:开通后需要先将机器人从频道移除，然后重新添加，方可生效。</li>
+      <li>公域机器人暂不支持申请，仅私域机器人可用，选择私域机器人后默认开通。</li>
+      <li>注意:开通后需要先将机器人从频道移除，然后重新添加，方可生效。</li>
     </ul>
   </div>
 </template>
